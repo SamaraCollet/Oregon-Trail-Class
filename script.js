6fab92e421477834342008f044e0bd24f8a0bf5c
Samara Collet <samaracollet@gmail.com>
2020-10-06 23:24:49 -0300

    Entrega: Converter o Oregon Trail para a Sintaxe de Classe

---
+++ script.js
@@ -0,0 +1,77 @@
+class Traveler {
+    constructor(name) {
+        this.name = name;
+        this.food = 1;
+        this.isHealthy = true;
+    }
+
+    hunt() {
+        this.food += 2
+    }
+
+    eat() {
+        if (this.food > 0) {
+            this.food += -1
+        }
+        if (this.food <= 0 && this.isHealthy != false) {
+            this.isHealthy = false
+        }
+    }
+}
+
+class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.passageiros = [];
+    }
+    getAvailableSeatCount() {
+        return this.capacity - this.passageiros.length
+    }
+
+    join(traveler) {
+        if (this.capacity > this.passageiros.length) {
+            this.passageiros.push(traveler)
+        }
+    }
+
+    shouldQuarantine() {
+        for (let i = 0; i < this.passageiros.length; i++) {
+            if (!this.passageiros.isHealthy) {
+                return true
+            }
+        }
+        return false
+    }
+
+    totalFood() {
+        let soma = 0
+        for (let i = 0; i < this.passageiros.length; i++) {
+            soma += this.passageiros[i].food
+        }
+        return soma
+    }
+}
+
+
+// Criar uma carroça que comporta 2 pessoas
+let wagon = new Wagon(2);
+// Criar três viajantes
+let henrietta = new Traveler('Henrietta');
+let juan = new Traveler('Juan');
+let maude = new Traveler('Maude');
+
+console.log(`${wagon.getAvailableSeatCount()} should be 2`);
+
+wagon.join(henrietta);
+console.log(`${wagon.getAvailableSeatCount()} should be 1`);
+
+wagon.join(juan);
+wagon.join(maude); // Não tem espaço para ela!
+console.log(`${wagon.getAvailableSeatCount()} should be 0`);
+
+henrietta.hunt(); // pega mais comida
+juan.eat();
+juan.eat(); // juan agora está com fome (doente)
+
+console.log(`${wagon.shouldQuarantine()} should be true since juan is sick`);
+console.log(`${wagon.totalFood()} should be 3`);
